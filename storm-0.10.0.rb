3bca9e9ca52df202c17b931c05fca6842b95d276
Nathan Trevivian <nathanandjoy@gmail.com>
2016-08-12 10:20:28 +0100

    Create storm-0.10.0.rb

---
+++ storm-0.10.0.rb
@@ -0,0 +1,19 @@
+class Storm < Formula
+  desc "Distributed realtime computation system to process data streams"
+  homepage "https://storm.apache.org"
+  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-0.10.0/apache-storm-0.10.0.tar.gz"
+  sha256 "1574c08d8cfb6bc7509c78871e98a535f1386d6ca50fa71c880c39e8800620bf"
+
+  bottle :unneeded
+
+  conflicts_with "stormssh", :because => "both install 'storm' binary"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/storm"
+  end
+
+  test do
+    system bin/"storm", "version"
+  end
+end
