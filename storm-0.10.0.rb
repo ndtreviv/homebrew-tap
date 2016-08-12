b96cf5a120982ef795c645710c994b7480dcef97
Nathan Trevivian <nathanandjoy@gmail.com>
2016-08-12 10:24:56 +0100

    Update storm-0.10.0.rb

--- storm-0.10.0.rb
+++ storm-0.10.0.rb
@@ -2,7 +2,7 @@ class Storm0100 < Formula
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
   url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-0.10.0/apache-storm-0.10.0.tar.gz"
-  sha256 "1574c08d8cfb6bc7509c78871e98a535f1386d6ca50fa71c880c39e8800620bf"
+  sha256 "066d1f5343333efd9187d7b850047cf9b3f63d885811e9fdd6e50f949b432f62"
 
   bottle :unneeded
 
